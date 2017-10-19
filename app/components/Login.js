0d31a7c00c734c9b3cd814f63d2f9d083ae309fe
sebtn <sdebedout@gmail.com>
2017-10-19 13:07:28 -0400

    Media queries added for login page

--- app/components/Login.js
+++ app/components/Login.js
@@ -4,12 +4,12 @@ import loginSVG from '../log_in.svg'
 export default class Login extends Component {
   render() {
     return (
-      <div className="login">
+      <div className="login phone-only tablet-only desktop-up">
         <h1><b> Welcome to Spot </b></h1>   
         <h2>Ideal App to browse the local spotify library and find the rigth sound-track.</h2>
         <h2>Press the logo to login into Spotify's services on-line.</h2>
         <h2>Find all your music in one place.</h2>
-        <a className="col-xs-12" href="/login" dangerouslySetInnerHTML={{__html: loginSVG}}></a>
+        <a href="/login" dangerouslySetInnerHTML={{__html: loginSVG}}></a>
       </div>
     )
   }
